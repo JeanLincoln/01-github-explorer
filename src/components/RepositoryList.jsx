ccfe355d54d1dfdeec6df566e253077f034cb6ab
Jean Lincoln <jeanlincol@gmail.com>
2022-07-04 19:33:16 -0700

    fazendo chamado HTTP com o useEffect e pre setando o useState

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,5 +1,6 @@
 import { RepositoryItem } from "./RepositoryItem"
 import '../styles/repositories.scss'
+import { useEffect, useState } from "react"
 
 const repository ={
     name:'unform',
@@ -8,6 +9,15 @@ const repository ={
 }
 
 export function RepositoryList(){
+const [repositories,setRepositories] = useState([])
+
+useEffect(()=>{
+    fetch('https://api.github.com/users/JeanLincoln/repos')
+    .then(res => res.json())
+    .then(data => setRepositories(data))
+},[])
+  
+
     return(
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
